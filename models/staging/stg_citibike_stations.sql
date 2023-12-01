5df2761c3680153cc61407a28f1777a78fe7e98f
chenjulie10@gmail.com <chenjulie10@gmail.com>
2023-12-01 04:28:10 +0000

    create stg_citibike_stations.sql file

---
+++ models/staging/stg_citibike_stations.sql
@@ -0,0 +1,22 @@
+SELECT 
+station_id,
+name as station_name,
+short_name,
+latitude,
+longitude,
+region_id,
+rental_methods,
+--ARRAY_TO_STRING(PARSE_JSON(rental_methods), ',') AS rental_options,
+capacity,
+eightd_has_key_dispenser,
+num_bikes_available,
+num_bikes_disabled,
+num_docks_available,
+num_docks_disabled,
+is_installed,
+is_renting,
+is_returning,
+eightd_has_available_keys,
+last_reported as last_reported_ts_et
+FROM `bigquery-public-data.new_york_citibike.citibike_stations`
+WHERE is_installed = TRUE -- filter for stations that are currently on the streets 
