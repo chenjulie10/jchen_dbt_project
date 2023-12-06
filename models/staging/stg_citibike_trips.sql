172d2f18b2263cff69e873c7f10de294fa371f7a
chenjulie10@gmail.com <chenjulie10@gmail.com>
2023-12-06 04:08:25 +0000

    created primary key for citibike trips

--- models/staging/stg_citibike_trips.sql
+++ models/staging/stg_citibike_trips.sql
@@ -1,20 +1,25 @@
-SELECT
--- need to make a primary key using dbt utils package 
-(tripduration)/60 as trip_length_mins,
-starttime as trip_start_ts,
-stoptime as trip_end_ts,
-start_station_id,
-start_station_name,
-start_station_latitude,
-start_station_longitude,
-end_station_id,
-end_station_name,
-end_station_latitude,
-end_station_longitude,
-bikeid as bike_id,
-usertype as user_type,
-birth_year,
-gender,
-customer_plan
+select
+    {{ dbt_utils.generate_surrogate_key(["bikeid", "starttime", "start_station_id"]) }}
+    as pk,
+    round((tripduration) / 60, 1) as trip_length_mins,
+    starttime as trip_start_ts,
+    date(starttime) as trip_start_date,
+    extract(hour from starttime) as trip_start_hr,
+    stoptime as trip_end_ts,
+    date(stoptime) as trip_end_date,
+    extract(hour from stoptime) as trip_end_hr,
+    cast(start_station_id as string) as start_station_id,
+    start_station_name,
+    start_station_latitude,
+    start_station_longitude,
+    cast(end_station_id as string) as end_station_id,
+    end_station_name,
+    end_station_latitude,
+    end_station_longitude,
+    bikeid as bike_id,
+    usertype as user_type,
+    birth_year,
+    gender,
+    customer_plan
 from `bigquery-public-data.new_york_citibike.citibike_trips`
-WHERE starttime is not null
+where starttime is not null
