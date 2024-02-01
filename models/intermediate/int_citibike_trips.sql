5aeb8629cc2a94b1108c2531c3897edb39883e66
chenjulie10@gmail.com <chenjulie10@gmail.com>
2024-02-01 21:18:40 +0000

    pivot by user type

--- models/intermediate/int_citibike_trips.sql
+++ models/intermediate/int_citibike_trips.sql
@@ -5,14 +5,16 @@ start_station_id,
 start_station_name,
 trip_start_ts,
 trip_start_date,
+trip_start_month,
 end_station_id,
 end_station_name,
 trip_end_ts,
 trip_end_date,
 trip_length_mins,
+user_type,
 DATE_DIFF(trip_end_date, trip_start_date, day) as trip_length_days,
 CASE WHEN start_station_id = end_station_id THEN 1 ELSE 0 END AS round_trip_yn
-from {{ ref('stg_citibike_trips') }}
+from {{ ref('stg_citibike_trips') }} t
 ) 
 
 --dedupe rows 
--- models/marts/station_trips_agg_by_year.sql
+++ models/marts/station_trips_agg_by_year.sql
@@ -1,20 +1,19 @@
-WITH trips AS (
+{% set users = ['Subscriber', 'Customer'] %}
+
 select 
 extract(year from trip_start_date) as trip_year,
 start_station_name,
-start_station_id,
 count(distinct pk) as total_trips_started,
-COUNT(DISTINCT CASE WHEN start_station_id = end_station_id THEN pk end) as total_round_trips
+COUNT(DISTINCT CASE WHEN start_station_id = end_station_id THEN pk end) as total_round_trips,
+
+  {% for rider in users %}
+    count(distinct CASE WHEN user_type = '{{ rider }}' THEN pk ELSE NULL end) as {{ rider}}_trips_started,
+    COUNT(DISTINCT CASE WHEN start_station_id = end_station_id AND user_type = '{{ rider }}' THEN pk ELSE NULL end) as {{ rider}}_round_trips
+    {% if not loop.last %}
+    , 
+    {% endif %}
+    {% endfor %}
+
 from {{ ref('int_citibike_trips') }} 
-group by 1,2,3
-order by 1,2,3
-)
-
-
-select
-trip_year,
-start_station_name,
-total_trips_started,
-total_round_trips
-from trips 
-    
+group by 1,2
+order by 1,2
