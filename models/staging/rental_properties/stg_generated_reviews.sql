ce65d4eec317b91967914269823f6a75c3537014
chenjulie10@gmail.com <chenjulie10@gmail.com>
2024-02-09 20:18:31 +0000

    commit

--- models/staging/rental_properties/stg_generated_reviews.sql
+++ models/staging/rental_properties/stg_generated_reviews.sql
@@ -1,7 +1,7 @@
 select
     id,
     listing_id,
-    coalesce(id, listing_id || review_scores) as review_pk,  -- if id is null, then return combination of listing ID and review date (assumes that there are no multiple rows with null ID and same listing ID and review score)
+    coalesce(id, listing_id || review_score) as review_pk,  -- if id is null, then return combination of listing ID and review score (assumes that there are no multiple rows with null ID and same listing ID and review score)
     review_score,
     review_date
 from {{ source("dbt_chenjulie10", "generated_reviews") }}
